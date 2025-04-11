91652335d650a70a0195af740319c98b48101976
Sondre <sondre.njastad.bjorn@gmail.com>
2025-04-11 08:57:26 +0200

    Mattesuppe-ting

---
+++ script.js
@@ -0,0 +1,14 @@
+// Ting vi vil ha med:
+/*
+- Animasjon av pyramiden og riktige tall markerte
+- Tid
+- Fremstille data på en eller annen måte (sammenligne både forskjellige algoritmer og ulike antall rader)
+- Flere algoritmer for å finne løsning (og sammenligne dem i data-ting, søylediagram)
+- Velge antall rader og velge intervall til verdiene i pyramiden (slider eller tekst?, kanskje helst slider)
+- kunne velge algritme (dropdown eller radiobutton)
+- Si noe om antall mulige utfall til ulike størrelser på pyramiden
+
+design:
+- tabell eller liste med antall muligheter, høyeste verdi og laveste verdi, (gjennomsnitt???)
+- Sektordiagram med fordeling av ulike verdier (0-10, 10-20, osv.) (laveste verdi som start, høyeste som slutt, dele i x forskjelige kategorier mellom)
+*/
