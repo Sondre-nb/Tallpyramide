7aa35aefc0303662652b5223127fe025d60e599b
Sondre <sondre.njastad.bjorn@gmail.com>
2025-04-11 10:38:47 +0200

    Ting i mattetimen fordi jeg ikke klarer å gjøre det jeg egentlig burde...

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ design:
 - tabell eller liste med antall muligheter, høyeste verdi og laveste verdi, (gjennomsnitt???)
 - Sektordiagram med fordeling av ulike verdier (0-10, 10-20, osv.) (laveste verdi som start, høyeste som slutt, dele i x forskjelige kategorier mellom)
 */
+
+let startSliderEl = document.querySelector("#start-slider")
+let displayVerdiStartEl = document.querySelector("#display-verdi-start")
+displayVerdiStartEl.innerHTML = startSliderEl.value
+startSliderEl.addEventListener("input", () => {
+    displayVerdiStartEl.innerHTML = startSliderEl.value
+})
+
+let sluttSliderEl = document.querySelector("#slutt-slider")
+let displayVerdiSluttEl = document.querySelector("#display-verdi-slutt")
+displayVerdiSluttEl.innerHTML = sluttSliderEl.value
+sluttSliderEl.addEventListener("input", () => {
+    displayVerdiSluttEl.innerHTML = sluttSliderEl.value
+})
+
+let raderSliderEl = document.querySelector("#rader-slider")
+let displayVerdiRaderEl = document.querySelector("#display-verdi-rader")
+displayVerdiRaderEl.innerHTML = raderSliderEl.value
+raderSliderEl.addEventListener("input", () => {
+    displayVerdiRaderEl.innerHTML = raderSliderEl.value
+})
