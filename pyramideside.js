9e74dbd5603fdb6b95b2a62e4772f2a678263dba
JulieDenKulie <julie-stang@outlook.com>
2025-04-22 14:26:43 +0200

    Fikset på javascript
    Flyttet på kode, pyramide eksisterer

--- pyramideside.js
+++ pyramideside.js
@@ -1,41 +1,3 @@
-// Ting vi vil ha med:
-/*
-- Animasjon av pyramiden og riktige tall markerte
-- Tid
-- Fremstille data på en eller annen måte (sammenligne både forskjellige algoritmer og ulike antall rader)
-- Flere algoritmer for å finne løsning (og sammenligne dem i data-ting, søylediagram)
-- Velge antall rader og velge intervall til verdiene i pyramiden (slider eller tekst?, kanskje helst slider)
-- kunne velge algritme (dropdown eller radiobutton)
-- Si noe om antall mulige utfall til ulike størrelser på pyramiden
-
-design:
-- tabell eller liste med antall muligheter, høyeste verdi og laveste verdi, (gjennomsnitt???)
-- Sektordiagram med fordeling av ulike verdier (0-10, 10-20, osv.) (laveste verdi som start, høyeste som slutt, dele i x forskjelige kategorier mellom)
-*/
-
-let startSliderEl = document.querySelector("#start-slider")
-let displayVerdiStartEl = document.querySelector("#display-verdi-start")
-displayVerdiStartEl.innerHTML = startSliderEl.value
-startSliderEl.addEventListener("input", () => {
-    displayVerdiStartEl.innerHTML = startSliderEl.value
-})
-
-let sluttSliderEl = document.querySelector("#slutt-slider")
-let displayVerdiSluttEl = document.querySelector("#display-verdi-slutt")
-displayVerdiSluttEl.innerHTML = sluttSliderEl.value
-sluttSliderEl.addEventListener("input", () => {
-    displayVerdiSluttEl.innerHTML = sluttSliderEl.value
-})
-
-let raderSliderEl = document.querySelector("#rader-slider")
-let displayVerdiRaderEl = document.querySelector("#display-verdi-rader")
-displayVerdiRaderEl.innerHTML = raderSliderEl.value
-raderSliderEl.addEventListener("input", () => {
-    displayVerdiRaderEl.innerHTML = raderSliderEl.value
-})
-
-
-
 let plasseringer;
 let høyesteVerdi2 = 0;
 
@@ -84,8 +46,6 @@ function finnRute2(plass, rad, verdi, plassering){
         sjekkVerdi2(verdi + tomPyramide[rad][plass], plassering)
     }
 }
-
-console.log(høyesteVerdi)
 
 let tomPyramide = []
 let tomSumPyramide = []
